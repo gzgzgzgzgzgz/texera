1cb906e66df337313723dcd7cc3a13b22b6fd576
Shengquan Ni <Hiseen@github.noreply.com>
2020-12-29 18:08:35 -0800

    update test accordingly

--- core/amber/src/test/scala/edu/uci/ics/amber/engine/architecture/messaginglayer/DataInputPortSpec.scala
+++ core/amber/src/test/scala/edu/uci/ics/amber/engine/architecture/messaginglayer/DataInputPortSpec.scala
@@ -3,26 +3,27 @@ package edu.uci.ics.amber.engine.architecture.messaginglayer
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.flatspec.AnyFlatSpec
 import com.softwaremill.macwire.wire
-import edu.uci.ics.amber.engine.architecture.messaginglayer.DataInputPort.InternalDataMessage
-import edu.uci.ics.amber.engine.common.ambermessage.WorkerMessage.DataPayload
-import edu.uci.ics.amber.engine.common.ambertag.neo.Identifier
+import edu.uci.ics.amber.engine.architecture.messaginglayer.DataInputPort.WorkflowDataMessage
+import edu.uci.ics.amber.engine.common.ambermessage.WorkerMessage.DataFrame
+import edu.uci.ics.amber.engine.common.ambertag.neo.VirtualIdentity.NamedActorVirtualIdentity
 import edu.uci.ics.amber.engine.common.tuple.ITuple
 
 class DataInputPortSpec extends AnyFlatSpec with MockFactory {
 
-  private val mockTupleProducer = mock[TupleProducer]
+  private val mockBatchToTupleConverter = mock[BatchToTupleConverter]
+  private val fakeID = NamedActorVirtualIdentity("testReceiver")
 
   "data input port" should "output data in FIFO order" in {
     val inputPort = wire[DataInputPort]
     val payloads = (0 until 4).map{
-      i => DataPayload(Array(ITuple(i)))
+      i => DataFrame(Array(ITuple(i)))
     }.toArray
     val messages = (0 until 4).map{
-      i => InternalDataMessage(Identifier.None,i,payloads(i))
+      i => WorkflowDataMessage(,i,payloads(i))
     }.toArray
     inSequence{
-      (mockTupleProducer.processDataEvents _).expects(Identifier.None,payloads.slice(0,3).toIterable)
-      (mockTupleProducer.processDataEvents _).expects(Identifier.None,Iterable(payloads(3)))
+      (mockBatchToTupleConverter.processDataEvents _).expects(fakeID,payloads.slice(0,3).toIterable)
+      (mockBatchToTupleConverter.processDataEvents _).expects(fakeID,Iterable(payloads(3)))
     }
 
     inputPort.handleDataMessage(messages(2))
@@ -33,11 +34,11 @@ class DataInputPortSpec extends AnyFlatSpec with MockFactory {
 
   "data input port" should "de-duplicate data " in {
     val inputPort = wire[DataInputPort]
-    val payload = DataPayload(Array(ITuple(0)))
-    val message = InternalDataMessage(Identifier.None,0,payload)
+    val payload = DataFrame(Array(ITuple(0)))
+    val message = WorkflowDataMessage(fakeID,0,payload)
     inSequence{
-      (mockTupleProducer.processDataEvents _).expects(Identifier.None,Iterable(payload))
-      (mockTupleProducer.processDataEvents _).expects(*,*).never
+      (mockBatchToTupleConverter.processDataEvents _).expects(fakeID,Iterable(payload))
+      (mockBatchToTupleConverter.processDataEvents _).expects(*,*).never
     }
     inputPort.handleDataMessage(message)
     inputPort.handleDataMessage(message)
