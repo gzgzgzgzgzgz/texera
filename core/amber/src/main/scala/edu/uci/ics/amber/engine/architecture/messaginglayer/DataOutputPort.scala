faeba557a2e87832a2a9b8891679e43549336fe0
Shengquan Ni <Hiseen@github.noreply.com>
2020-12-31 22:35:30 -0800

    add debug log

--- core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/messaginglayer/DataOutputPort.scala
+++ core/amber/src/main/scala/edu/uci/ics/amber/engine/architecture/messaginglayer/DataOutputPort.scala
@@ -3,6 +3,7 @@ package edu.uci.ics.amber.engine.architecture.messaginglayer
 import java.util.concurrent.atomic.AtomicLong
 
 import akka.actor.ActorRef
+import com.typesafe.scalalogging.LazyLogging
 import edu.uci.ics.amber.engine.architecture.messaginglayer.DataInputPort.WorkflowDataMessage
 import edu.uci.ics.amber.engine.architecture.messaginglayer.NetworkSenderActor.{
   NetworkSenderActorRef,
@@ -17,9 +18,11 @@ import scala.collection.mutable
   * The internal logic can send data messages to other actor without knowing
   * where the actor is and without determining the sequence number.
   */
-class DataOutputPort(selfID: ActorVirtualIdentity, networkSenderActor: NetworkSenderActorRef) {
+class DataOutputPort(selfID: ActorVirtualIdentity, networkSenderActor: NetworkSenderActorRef)
+    extends LazyLogging {
 
   private val idToSequenceNums = new mutable.AnyRefMap[ActorVirtualIdentity, AtomicLong]()
+  private var c = 0
 
   def sendTo(to: ActorVirtualIdentity, event: DataPayload): Unit = {
     val msg = WorkflowDataMessage(
@@ -27,6 +30,10 @@ class DataOutputPort(selfID: ActorVirtualIdentity, networkSenderActor: NetworkSe
       idToSequenceNums.getOrElseUpdate(to, new AtomicLong()).getAndIncrement(),
       event
     )
+    c += 1
+    if (c % 20 == 0) {
+      logger.info(s"send $msg to $to")
+    }
     networkSenderActor ! SendRequest(to, msg)
   }
 
